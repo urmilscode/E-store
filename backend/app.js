1a03660af0a30157507f39583c2c4e26a9cb1c9b
urmil <ucodes28@gmail.com>
2021-01-27 19:08:25 -0600

    configuring the environment variables

--- backend/app.js
+++ backend/app.js
@@ -3,16 +3,19 @@
 const express = require('express');
 const app = express();
 
+require('dotenv/config');
 
+const api = process.env.API_URL
 
 //specifying the initial route of our application
 
-app.get('/', (req,res) => {
+app.get(api+'/products', (req,res) => {
     res.send('the bundesliga season starts today!!!');
 })
 
 
 
 app.listen(3000, () => {
+    console.log(api);
     console.log('app is listening at port 3000');
 })
